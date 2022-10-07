e96194d1380d3b5284cf3f7bdb22ddd84ef0f3b1
Liviu Deleu <liviu.deleu.2002@gmail.com>
2022-10-07 12:28:39 +0300

    e bun

--- script.js
+++ script.js
@@ -2,18 +2,13 @@ function insert(num) {
     document.form.textview.value  = document.form.textview.value += num;
 }
 
-// function clearAll () {
-//     document.form.textview.value = "";
-// }
+function clearr() {
+    document.form.textview.value = "";
+}
 
-// function backspace () {
-//     let delSign = document.form.textview.value;
-//     document.form.textview.value = delSign.substring(0, delSign.length-1);
-// }
-
-// function equal() {
-//     let calcValue = document.form.textview.value;
-//     if(calcValue) {
-//         document.form.textview.value = eval(calcValue);
-//     }
-// }
+function egal() {
+    let calcValue = document.form.textview.value;
+    if(calcValue) {
+        document.form.textview.value = eval(calcValue);
+    }
+}
